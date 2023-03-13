0012b4c7b2c8fcb30b16cbbd27ca38ec3bad8ce5
yoshiki-bull <dnttz118@outlook.jp>
2023-03-13 22:41:08 +0900

    Setup

---
+++ src/main/java/com/udemy/videolist/VideoListApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.videolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/videolist/VideoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.videolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
