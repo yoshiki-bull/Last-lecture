51c8751042db88626b1d98154b280e61b8216cbb
yoshiki-bull <dnttz118@outlook.jp>
2023-03-18 17:31:57 +0900

    sqlファイルの修正

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,13 +3,14 @@ DROP TABLE IF EXISTS videos;
 CREATE TABLE videos (
   video_id int unsigned AUTO_INCREMENT COMMENT "動画ID",
   video_title VARCHAR(100) NOT NULL COMMENT "動画タイトル",
+  instructor VARCHAR(100) NOT NULL COMMENT "作者",
   video_language VARCHAR(100) NOT NULL COMMENT "動画の言語",
   price CHAR(10) NOT NULL COMMENT "有料か無料か",
   video_price INT(5) NOT NULL COMMENT "動画の値段",
   PRIMARY KEY(video_id)
  ) COMMENT="動画一覧";
 
-INSERT INTO videos (video_title, video_language, price, video_price) VALUES ('もう怖くないGit!', 'ja', 'price-paid', 12000);
-INSERT INTO videos (video_title, video_language, price, video_price) VALUES ('もう怖くないLinuxコマンド。', 'ja', 'price-paid', 12000);
-INSERT INTO videos (video_title, video_language, price, video_price) VALUES ('ゼロから実践するAWS。', 'ja', 'price-free', 0);
-INSERT INTO videos (video_title, video_language, price, video_price) VALUES ('Java Programming Masterclass updated to Java 17', 'en', 'price-free', 0);
+INSERT INTO videos (video_title, video_language, instructor, price, video_price) VALUES ('もう怖くないGit!', '山浦', 'Japanese', 'price-paid', 12000);
+INSERT INTO videos (video_title, video_language, instructor, price, video_price) VALUES ('もう怖くないLinuxコマンド', '田中', 'Japanese', 'price-paid', 12000);
+INSERT INTO videos (video_title, video_language, instructor, price, video_price) VALUES ('Enjoy Programming', 'Jonny', 'English', 'price-free', 0);
+INSERT INTO videos (video_title, video_language, instructor, price, video_price) VALUES ('Java Programming 17', 'Mike', 'English', 'price-free', 0);
