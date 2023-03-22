edae5085d895a48cce947adb9f1aad39c569499d
yoshiki-bull <dnttz118@outlook.jp>
2023-03-22 21:11:05 +0900

    例外ハンドリング専用のクラスを作成およびそれに伴う修正
    例外ハンドリング専用のクラスをつくり、そのクラスをControllerに継承することで例外処理を実現しました。

--- src/main/java/com/udemy/videolist/application/VideoController.java
+++ src/main/java/com/udemy/videolist/application/VideoController.java
@@ -2,39 +2,22 @@ package com.udemy.videolist.application;
 
 import com.udemy.videolist.model.Video;
 import com.udemy.videolist.service.VideoService;
-import com.udemy.videolist.service.exception.VideoNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
+import com.udemy.videolist.service.exception.VideoNotFoundExceptionHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/videos")
 @RequiredArgsConstructor
-public class VideoController {
+public class VideoController extends VideoNotFoundExceptionHandler {
 
     private final VideoService videoService;
 
     @GetMapping("/{id}")
     public Video findByVideoId(@PathVariable("id") int videoId) {
         return videoService.findByVideoId(videoId);
-    }
-
-    @ExceptionHandler(value = VideoNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handlerNoResourceFound(VideoNotFoundException e, HttpServletRequest request) {
-
-        Map<String, String> body = Map.of(
-        "timestamp", ZonedDateTime.now().toString(),
-                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
-                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
-                "message", e.getMessage(),
-                "path", request.getRequestURI());
-        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/search")
---
+++ src/main/java/com/udemy/videolist/service/exception/VideoNotFoundExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.udemy.videolist.service.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+public class VideoNotFoundExceptionHandler {
+
+    @ExceptionHandler(value = VideoNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerNoResourceFound(VideoNotFoundException e, HttpServletRequest request) {
+
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message", e.getMessage(),
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+    }
+}
