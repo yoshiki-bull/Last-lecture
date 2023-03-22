3bbf015b7723be57920dc99f829306d23dd37657
yoshiki-bull <dnttz118@outlook.jp>
2023-03-22 22:21:03 +0900

    紛らわしかったカラム名を変更
    price -> priceState
    video_price -> price

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -5,12 +5,12 @@ CREATE TABLE videos (
   title VARCHAR(100) NOT NULL COMMENT "動画タイトル",
   instructor VARCHAR(100) NOT NULL COMMENT "作者",
   language VARCHAR(100) NOT NULL COMMENT "動画の言語",
-  price CHAR(10) NOT NULL COMMENT "有料か無料か",
-  video_price INT(5) NOT NULL COMMENT "動画の値段",
+  priceState CHAR(10) NOT NULL COMMENT "有料か無料か",
+  price INT(5) NOT NULL COMMENT "動画の値段",
   PRIMARY KEY(id)
  ) COMMENT="動画一覧";
 
-INSERT INTO videos (title, instructor, language, price, video_price) VALUES ('もう怖くないGit!', '山浦', 'Japanese', 'price-paid', 12000);
-INSERT INTO videos (title, instructor, language, price, video_price) VALUES ('もう怖くないLinuxコマンド', '田中', 'Japanese', 'price-free', 0);
-INSERT INTO videos (title, instructor, language, price, video_price) VALUES ('Enjoy Programming', 'Jonny', 'English', 'price-free', 0);
-INSERT INTO videos (title, instructor, language, price, video_price) VALUES ('Java Programming 17', 'Mike', 'English', 'price-paid', 12000);
+INSERT INTO videos (title, instructor, language, priceState, price) VALUES ('もう怖くないGit!', '山浦', 'Japanese', 'price-paid', 12000);
+INSERT INTO videos (title, instructor, language, priceState, price) VALUES ('もう怖くないLinuxコマンド', '田中', 'Japanese', 'price-free', 0);
+INSERT INTO videos (title, instructor, language, priceState, price) VALUES ('Enjoy Programming', 'Jonny', 'English', 'price-free', 0);
+INSERT INTO videos (title, instructor, language, priceState, price) VALUES ('Java Programming 17', 'Mike', 'English', 'price-paid', 12000);
