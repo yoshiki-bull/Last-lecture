b3fe2129eb26e42ae3009f908baa593c03779021
yoshiki-bull <dnttz118@outlook.jp>
2023-03-23 15:06:04 +0900

    インポートにおけるワイルドカードの使用を修正
    何をインポートしてるかわかるようにするため。

--- src/main/java/com/udemy/videolist/application/VideoController.java
+++ src/main/java/com/udemy/videolist/application/VideoController.java
@@ -3,7 +3,11 @@ package com.udemy.videolist.application;
 import com.udemy.videolist.model.Video;
 import com.udemy.videolist.service.VideoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
