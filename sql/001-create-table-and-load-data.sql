4a8eb26aa3f09e36a4f36616978ed7a2d9ce13cb
yoshiki-bull <dnttz118@outlook.jp>
2023-03-23 14:27:02 +0900

    有料か無料かを扱うカラムの型および名前の変更
    CHAR -> BOOLEAN
    price_state -> is_free

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -5,12 +5,12 @@ CREATE TABLE videos (
   title VARCHAR(100) NOT NULL COMMENT "動画タイトル",
   instructor VARCHAR(100) NOT NULL COMMENT "作者",
   language VARCHAR(100) NOT NULL COMMENT "動画の言語",
-  price_state CHAR(10) NOT NULL COMMENT "有料か無料か",
+  is_free BOOLEAN NOT NULL COMMENT "有料か無料か",
   price INT(5) NOT NULL COMMENT "動画の値段",
   PRIMARY KEY(id)
  ) COMMENT="動画一覧";
 
-INSERT INTO videos (title, instructor, language, price_state, price) VALUES ('もう怖くないGit!', '山浦', 'Japanese', 'price-paid', 12000);
-INSERT INTO videos (title, instructor, language, price_state, price) VALUES ('もう怖くないLinuxコマンド', '田中', 'Japanese', 'price-free', 0);
-INSERT INTO videos (title, instructor, language, price_state, price) VALUES ('Enjoy Programming', 'Jonny', 'English', 'price-free', 0);
-INSERT INTO videos (title, instructor, language, price_state, price) VALUES ('Java Programming 17', 'Mike', 'English', 'price-paid', 12000);
+INSERT INTO videos (title, instructor, language, is_free, price) VALUES ('もう怖くないGit!', '山浦', 'Japanese', false, 12000);
+INSERT INTO videos (title, instructor, language, is_free, price) VALUES ('もう怖くないLinuxコマンド', '田中', 'Japanese', true, 0);
+INSERT INTO videos (title, instructor, language, is_free, price) VALUES ('Enjoy Programming', 'Jonny', 'English', true, 0);
+INSERT INTO videos (title, instructor, language, is_free, price) VALUES ('Java Programming 17', 'Mike', 'English', false, 12000);
