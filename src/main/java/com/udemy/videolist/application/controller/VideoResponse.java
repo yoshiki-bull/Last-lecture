ce96c373b65ea990d2d95f4c6941df64f8d136b9
yoshiki-bull <dnttz118@outlook.jp>
2023-03-27 11:11:16 +0900

    検索時にidも返すように修正

--- src/main/java/com/udemy/videolist/application/controller/VideoResponse.java
+++ src/main/java/com/udemy/videolist/application/controller/VideoResponse.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 public class VideoResponse {
 
+    private int id;
+
     private String title;
 
     private String instructor;
@@ -15,6 +17,7 @@ public class VideoResponse {
     private int price;
 
     public VideoResponse(Video video) {
+        this.id = video.getId();
         this.title = video.getTitle();
         this.instructor = video.getInstructor();
         this.language = video.getLanguage();
