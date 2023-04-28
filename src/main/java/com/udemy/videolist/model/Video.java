66065266239af27d608a4b0ae0835eb197d3789c
yoshiki-bull <dnttz118@outlook.jp>
2023-04-28 14:34:17 +0900

    DataSet Error

--- src/main/java/com/udemy/videolist/model/Video.java
+++ src/main/java/com/udemy/videolist/model/Video.java
@@ -1,10 +1,12 @@
 package com.udemy.videolist.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Video {
 
   private int id;
--- src/test/java/com/udemy/videolist/repository/VideoMapperTest.java
+++ src/test/java/com/udemy/videolist/repository/VideoMapperTest.java
@@ -2,16 +2,17 @@ package com.udemy.videolist.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.junit5.api.DBRider;
 import com.udemy.videolist.model.Video;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
-//@DBRider
+@DBRider
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class VideoMapperTest {
@@ -19,7 +20,7 @@ class VideoMapperTest {
   @Autowired
   VideoMapper videoMapper;
 
-  @Test
+  /*@Test
   @Sql(
       scripts = {"classpath:/sqlannotation/delete-videos.sql", "classpath:/sqlannotation/insert-videos.sql"},
       executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
@@ -34,9 +35,9 @@ class VideoMapperTest {
             new Video(1, "もう怖くないGit!", "山浦", "Japanese", false, 12000),
             new Video(2, "The Web Developer Bootcamp", "Colt", "English", false, 12000)
         );
-  }
+  }*/
 
-  /*
+
   @Test
   @DataSet(value = "videoList.yml")
   @Transactional
@@ -49,5 +50,5 @@ class VideoMapperTest {
             new Video(1, "もう怖くないGit!", "山浦", "Japanese", false, 12000),
             new Video(2, "The Web Developer Bootcamp", "Colt", "English", false, 12000)
         );
-  }*/
+  }
 }
