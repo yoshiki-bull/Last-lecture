d46b811c414f73b49b45fe9ef34325939c5a71ba
yoshiki-bull <120367482+yoshiki-bull@users.noreply.github.com>
2023-05-25 15:15:40 +0900

    ビデオ情報更新時に返すレスポンスクラスをrecordクラスに変更

--- src/main/java/com/udemy/videolist/application/controller/VideoUpdateResponse.java
+++ src/main/java/com/udemy/videolist/application/controller/VideoUpdateResponse.java
@@ -3,29 +3,22 @@ package com.udemy.videolist.application.controller;
 import com.udemy.videolist.model.Video;
 import lombok.Getter;
 
-@Getter
-public class VideoUpdateResponse {
+record VideoUpdateResponse(
+    String message,
+    String title,
+    String instructor,
+    String language,
+    Boolean isFree,
+    int price) {
 
-  private final String message;
-
-  private final String title;
-
-  private final String instructor;
-
-  private final String language;
-
-  private final Boolean isFree;
-
-  private final int price;
-
-  protected VideoUpdateResponse(Video video) {
-    this.message = "video successfully updated";
-    this.title = video.getTitle();
-    this.instructor = video.getInstructor();
-    this.language = video.getLanguage();
-    this.isFree = video.getIsFree();
-    this.price = video.getPrice();
+  public VideoUpdateResponse(Video video) {
+    this(
+        "video successfully updated",
+        video.getTitle(),
+        video.getInstructor(),
+        video.getLanguage(),
+        video.getIsFree(),
+        video.getPrice()
+    );
   }
-
-  ;
 }
